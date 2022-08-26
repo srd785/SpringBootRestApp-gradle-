2bca53f82fbf5cadbd62d0827b296d4c0d4a9837
srd785 <107203103+srd785@users.noreply.github.com>
2022-08-27 00:11:49 +0530

    Update MyController.java

--- src/main/java/com/kkjavatutorials/SpringBootRestApp/controller/MyController.java
+++ src/main/java/com/kkjavatutorials/SpringBootRestApp/controller/MyController.java
@@ -8,7 +8,7 @@ public class MyController {
 	
 	@GetMapping(path = "/welcome")
 	public String welcome() {
-		return "Welcome Shubham , Shweta, Sitesh, Soubhagya, Ankit";
+		return "Welcome to Soubhagya";
 	}
 
 }
